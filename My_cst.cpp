1c1cdfd0f6ae2a131dc54c80100b7f6c94201c0d
Adrien GIDE <adrien.gide@outlook.com>
2019-05-27 17:34:03 +0900

    Union A pas encore ouf

--- My_cst.cpp
+++ My_cst.cpp
@@ -11,22 +11,22 @@
 #include <string>
 
 using namespace sdsl;
-
+using namespace std;
 
 void My_cst::repeat(char* name_file)
 {
     name_f = name_file;
     construct(cst, name_file,1);
-    
-    auto root = cst.root();
-    
-//    typedef const_bottom_up_iterator iterator;
-//    iterator begin = cst.begin_bottom_up();
-//    iterator end   = cst.end_bottom_up();
 
     typedef cst_bfs_iterator<cst_t> iterator;
     iterator begin = iterator(&cst, cst.root());
     iterator end   = iterator(&cst, cst.root(), true, true);
+    
+    vector<pair<char,int>> vec(MAX_NODES);
+    
+    vector<vector<pair<char,int>> > vec2d(MAX_CHILDREN,vec);
+    
+    pair_array = vec2d;
     
     for(iterator it=begin; it != end; it++)
     {
@@ -40,19 +40,19 @@ void My_cst::repeat(char* name_file)
 //                    A(child,c);
 //                    if( map_pos.count(cst.id(child))==1 )
                     A(*it,c);
-                    if( map_pos.count(cst.id(*it))==1 )
-                    {
-//                        std::cout << "id node : " << cst.id(child) << " - pair : ( " << map_pos.at(cst.id(child)).first << ", " << map_pos.at(cst.id(child)).second << ")" << std::endl;
-////                        std::cout << "id node : " << cst.id(*it) << " - pair : ( " << map_pos.at(cst.id(*it)).first << ", " << map_pos.at(cst.id(*it)).second << ")" << std::endl;
+//                    if( map_pos.count(cst.id(*it))==1 )
+//                    {
+//                        cout << "id node : " << cst.id(child) << " - pair : ( " << map_pos.at(cst.id(child)).first << ", " << map_pos.at(cst.id(child)).second << ")" << endl;
+////                        cout << "id node : " << cst.id(*it) << " - pair : ( " << map_pos.at(cst.id(*it)).first << ", " << map_pos.at(cst.id(*it)).second << ")" << endl;
 //                        for(int d=0; d<128; d++)
 //
 //                            A(sib,d);
 //                            if (d!=c && !map_pos.contains(cst.id(child)))
 //                            {
-//                                std::cout << "pair 2: ( ( " << position.first << ", " << position.second << ")" << std::endl;
+//                                cout << "pair 2: ( ( " << position.first << ", " << position.second << ")" << endl;
 //                            }
 //
-                    }
+//                    }
                 }
                
 //                for(int i=1; i<=cst.depth(child);i++)
@@ -60,72 +60,101 @@ void My_cst::repeat(char* name_file)
 //                    for(int j=1; j<=cst.depth(sib);j++)
 //
 //                        char c = cst.edge(child,i);
-//                        std::cout << c << " - " ;
+//                        cout << c << " - " ;
 //                        char d = cst.edge(sib,j);
-//                        std::cout << d << " " ;
+//                        cout << d << " " ;
 //                        if(c!=d)
 //                        {
-//                            std::cout << "result: ( (" << i << "," << i+cst.depth(*it)-1 << "), (" << j << "," << j+cst.depth(*it)-1 << ")" << std::endl;
+//                            cout << "result: ( (" << i << "," << i+cst.depth(*it)-1 << "), (" << j << "," << j+cst.depth(*it)-1 << ")" << endl;
 //
 //                        }
 //
 //
 //            }
     }
-    
 }
 
-void My_cst::A(node_type v, char c)
+void My_cst::A(node_type v, char c,int id)
 {
     if(cst.is_leaf(v))
     {
         if(get_i(c,v))
         {
-            map_pos.insert(std::make_pair(cst.id(v),position));
+            
+            int place;
+            if(cst.id(v)==0)
+            {
+                place = 0;
+            }
+            else
+            {
+                place = cst.id(cst.parent(v));
+            }
+            vector<pair<char,int>> vect;
+            vect.push_back(position);
+
+            
+            pair_array.at(place) = vect;
+            pair_array.at(cst.id(v)) = vect;
+
+//            vector<vector<pair<char,int>> >::iterator it = pair_array.insert(pair_array.begin()+place+1,vect);
+//            vector<vector<pair<char,int>> >::iterator it2 = pair_array.insert(pair_array.begin()+cst.id(v)+1,vect);
+            
         }
     }
     else
         for(auto& child : cst.children(v))
-            A(child, c);
-    
+        {
+            id = cst.id(child);
+            A(child, c,id);
+        }
+ 
 }
 
 bool My_cst::get_i(char c, node_type v)
 {
-    std::ifstream is(name_f);
-    std::string origin( (std::istreambuf_iterator<char>(is) ), (std::istreambuf_iterator<char>()  ));
+    ifstream is(name_f);
+    string origin( (istreambuf_iterator<char>(is) ), (istreambuf_iterator<char>()  ));
                        
     int length = origin.size();
     int index = length - cst.depth(v) + 1;
                        
     if( c == origin[index - 1] )
     {
-        std::cout << origin[index - 1] <<std::endl;
-        position = std::make_pair(c,index+1);
+        cout << origin[index - 1] <<endl;
+        position = make_pair(c,index+1);
         
         for(int i=index; i<length; i++)
-            std::cout << origin[i];
-        std::cout<< " : index "<< cst.id(v)<<std::endl;
+            cout << origin[i];
+        cout<< " : node "<< cst.id(v)<<endl;
         return true;
     }
-    
     is.close();
     return false;
 }
 
 void My_cst::printlist()
 {
-    for (std::map<size_type, std::pair<char,int>>::iterator it=map_pos.begin(); it!=map_pos.end(); ++it)
-    {
-        std::cout << "id node : " << it->first << " - pair : ( " << it->second.first << ", " << it->second.second << ")" << std::endl;
+//    for (map<size_type, list<pair<char,int>>>::iterator it=map_pos.begin(); it!=map_pos.end(); it++)
+//    {
+//        cout << "\n id node : " << it->first << endl;
+//
+//        for (pair<char,int> p : it->second)
+//            cout << "  pair : (" << p.first << ", " << p.second << ")" << endl;
+//    }
+    for(int i=0;i<pair_array.size(); i++) {
+        for (int j=0;j<pair_array[i].size(); j++)
+            cout << "  pair : (" << pair_array[i][j].first << ", " << pair_array[i][j].second << ")" << endl;
+        cout << endl;
     }
+ 
 }
 
 int main(int argc, char* argv[])
 {
     if(argc < 2)
     {
-        std::cout << "usage: " << argv[0] << " file" << std::endl;
+        cout << "usage: " << argv[0] << " file" << endl;
         return 1;
     }
     
--- My_cst.hpp
+++ My_cst.hpp
@@ -8,26 +8,30 @@
 #ifndef repeat_hpp
 #define repeat_hpp
 
-#define NOT_FOUND -1
+#define MAX_NODES 50
+#define MAX_CHILDREN 50
+
 
 #include <stdio.h>
 #include <iostream>
 #include <sdsl/suffix_trees.hpp>
-
+#include <vector>
 
 typedef sdsl::cst_sct3<> cst_t;
 
 class My_cst : public cst_t
 {
 private:
+    
     cst_t cst;
     std::pair<char,int> position;
-    std::map<size_type, std::pair<char,int>> map_pos;
+//    std::map<size_type, std::list<std::pair<char,int>>> map_pos;
+    std::vector< std::vector<std::pair<char,int> > > pair_array;
     const char* name_f;
-    char* suff;
     
 public:
-    void A(node_type v, char c);
+    
+    void A(node_type v, char c,int id=0);
     void repeat(char* name_file);
     bool get_i(char c,node_type v);
     void printlist();
