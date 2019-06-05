b7291bfc48f87a13d735a566604f347377782827
Adrien GIDE <adrien.gide@outlook.com>
2019-06-05 16:39:48 +0900

    krkr

--- test.cpp
+++ test.cpp
@@ -9,34 +9,34 @@
 #include "catch.hpp"
 #include "My_cst.hpp"
 #include <time.h>
-//
-//TEST_CASE("One file")
-//{
-//    My_cst test;
-//    clock_t time;
-//    time = clock();
-//
-//    SECTION( "Simple" ) {
-//        test.repeat("Test files/Test1.txt");
-//        test.printlist();
-//    }
-//    SECTION( "Complicated" ) {
-//        test.repeat("Test files/Test2.txt",3);
-//        test.printlist();
-//    }
-//    SECTION( "Multiple" ) {
-//        test.repeat("Test files/Test3.txt",8);
-//        test.printlist();
-//    }
-//    SECTION( "Code" ) {
-//        test.repeat("Test files/Test4.js.txt",274);
-//        test.printlist();
-//    }
-//
-//    std::cout << "\nTime: " << ( clock() - time )/ (double) CLOCKS_PER_SEC << " second(s)"<< std::endl;
-//    std::cout << "\n------------------------------------------------------------\n"<<std::endl;
-//
-//}
+
+TEST_CASE("One file")
+{
+    My_cst test;
+    clock_t time;
+    time = clock();
+
+    SECTION( "Simple" ) {
+        test.repeat("Test files/Test1.txt");
+        test.printlist();
+    }
+    SECTION( "Complicated" ) {
+        test.repeat("Test files/Test2.txt",3);
+        test.printlist();
+    }
+    SECTION( "Multiple" ) {
+        test.repeat("Test files/Test3.txt",8);
+        test.printlist();
+    }
+    SECTION( "Code" ) {
+        test.repeat("Test files/Test4.js.txt",274);
+        test.printlist();
+    }
+
+    std::cout << "\nTime: " << ( clock() - time )/ (double) CLOCKS_PER_SEC << " second(s)"<< std::endl;
+    std::cout << "\n------------------------------------------------------------\n"<<std::endl;
+
+}
 
 TEST_CASE("Multiple files")
 {
@@ -45,31 +45,31 @@ TEST_CASE("Multiple files")
     time = clock();
     std::set<std::string> files;
    
-//    SECTION( "2 files - Simple single strings" ) {
-//
-//        files.insert("Test files/Test5_1.txt");
-//        files.insert("Test files/Test5_2.txt");
-//        test.compare(files);
-//        test.printlist();
-//    }
-//    SECTION( "2 files - Complicated single strings" ) {
-//        files.insert("Test files/Test6_1.txt");
-//        files.insert("Test files/Test6_2.txt");
-//        test.compare(files,3);
-//        test.printlist();
-//    }
+    SECTION( "2 files - Simple single strings" ) {
+
+        files.insert("Test files/Test5_1.txt");
+        files.insert("Test files/Test5_2.txt");
+        test.compare(files);
+        test.printlist();
+    }
+    SECTION( "2 files - Complicated single strings" ) {
+        files.insert("Test files/Test6_1.txt");
+        files.insert("Test files/Test6_2.txt");
+        test.compare(files,3);
+        test.printlist();
+    }
     SECTION( "2 files - Multiple lines" ) {
         files.insert("Test files/Test7_2.txt");
         files.insert("Test files/Test7_1.txt");
-        test.compare(files,8);
+        test.compare(files,10);
         test.printlist();
     }
-//    SECTION( "2 files - Source code files" ) {
-//        files.insert("Test files/Test8_1.cs");
-//        files.insert("Test files/Test8_2.cs");
-//        test.compare(files,20);
-//        test.printlist();
-//    }
+    SECTION( "2 files - Source code files" ) {
+        files.insert("Test files/Test8_1.cs");
+        files.insert("Test files/Test8_2.cs");
+        test.compare(files,20);
+        test.printlist();
+    }
 
     std::cout << "\nTime: " << ( clock() - time )/ (double) CLOCKS_PER_SEC << " second(s)"<< std::endl;
     std::cout << "\n------------------------------------------------------------\n"<<std::endl;
